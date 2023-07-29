affa1fa99ce8830ecdc1cc5c9d705bb71ff0d535
Becky <bjentwistle@gmail.com>
2023-07-29 13:18:54 +0100

    corrected spelling mistakes

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -5,7 +5,7 @@ db.dropDatabase()
 db.users.insertMany([
 
 {name: "Jack", highScore: 15000},
-{name: "Collete", highScore: 6000},
+{name: "Colette", highScore: 6000},
 {name: "Alex", highScore: 7000},
 {name: "John", highScore: 8000}
 

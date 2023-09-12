40f41c4c762cc7db52163b49e2ae33a30b357ea5
Becky <bjentwistle@gmail.com>
2023-09-12 15:21:14 +0100

    added link to mongodb atlas cluster

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const app = express();
 const cors = require("cors");
 
 //const url = 'mongodb://localhost:27017'
+//This new url points to my MongodDB Atlas account.
 const url = "mongodb+srv://players:3uuRu7uhGcHKA@cluster0.0uqzzoz.mongodb.net/?appName=mongosh+1.10.6"
 const MongoClient = require('mongodb').MongoClient;
 const createRouter = require('./helpers/create_router.js');
